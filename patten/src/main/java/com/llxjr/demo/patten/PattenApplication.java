a6a5cd8a1416dd73151651039330e0e738a52026
liulz <liu_linzhi@creotechco.com>
2020-12-31 10:12:37 +0800

    设计模式外框

---
+++ patten/src/main/java/com/llxjr/demo/patten/PattenApplication.java
@@ -0,0 +1,13 @@
+package com.llxjr.demo.patten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PattenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PattenApplication.class, args);
+    }
+
+}
---
+++ patten/src/test/java/com/llxjr/demo/patten/PattenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.llxjr.demo.patten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PattenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
